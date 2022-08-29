a45f064a23e90487d2b7811f8c82d3db0e429f26
Styopa Groznov <groznov3@gmail.com>
2022-08-30 00:11:06 +0300

    Described disappearer

---
+++ Assets/Scripts/Disappearer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+public class Disappearer : MonoBehaviour
+{
+    public event Action OnDisappear;
+
+    public const float DisappearDistance = 25;
+
+    private Character _character;
+
+    private void Awake() => _character = FindObjectOfType<Character>();
+
+    private void Update()
+    {
+        if (transform.position.x < _character.transform.position.x - DisappearDistance)
+        {
+            OnDisappear?.Invoke();
+            gameObject.SetActive(false);
+        }
+    }
+}
