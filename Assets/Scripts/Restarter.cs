1067f575bdf03b3634d44bca9bb844f34ea73bc9
Styopa Groznov <groznov3@gmail.com>
2022-08-30 00:23:45 +0300

    Described restarter

---
+++ Assets/Scripts/Restarter.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using Motion;
+
+public class Restarter : MonoBehaviour
+{
+    private Faller _faller;
+
+    private void Awake()
+    {
+        _faller = FindObjectOfType<Faller>();
+        _faller.OnFalled += Activate;
+        gameObject.SetActive(false);
+    }
+
+    public void Restart() => SceneManager.LoadScene(0);
+
+    private void Activate() => gameObject.SetActive(true);
+}
