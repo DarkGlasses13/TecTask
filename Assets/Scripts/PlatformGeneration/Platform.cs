b728ae2b1990af5c1f7d0037e659837f50aeff16
Styopa Groznov <groznov3@gmail.com>
2022-08-30 00:12:19 +0300

    Enabled platform pool

---
+++ Assets/Scripts/PlatformGeneration/Platform.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace PlatformGeneration
+{
+    public class Platform : MonoBehaviour
+    {
+        public const float Height = 10;
+
+        public float Width => transform.localScale.x;
+        public float AbysWidth { get; private set; }
+
+        public void SetSize(float size) => transform.localScale = new(size, Height, 1);
+
+        public void SetAbysWidth(float width) => AbysWidth = width;
+    }
+}
